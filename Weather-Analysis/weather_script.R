390153aa5e20043fc919a68ac27727ed5cff7a19
abdullahemad190634-jpg <abdullahemad190634@gmail.com>
2026-02-06 15:17:29 +0200

    Update weather_script.R

--- Weather-Analysis/weather_script.R
+++ Weather-Analysis/weather_script.R
@@ -1,6 +1,8 @@
 city <- c("cairo","alex","Aswan")
 temp <- c(25,20,35)
 weather_df <- data.frame(city,temp)
+weather_df$humidity <- c(50,70,20)
 library(ggplot2)
-ggplot(weather_df, aes( x="city", y="temp",fill=city))+geom_bar(stat="identity")+labs(title="Average Temperature per City")
+ggplot(weather_df, aes( x=city, y=temp,fill=city))+geom_bar(stat="identity")+labs(title="Average Temperature per City")
+ggplot(weather_df, aes( x=temo, y=humidity,label=city))+geom_point() + geom_text(vjust=-1) + labs(title="Temperature vs Humidity Analysis")
 
